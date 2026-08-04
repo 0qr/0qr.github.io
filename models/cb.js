60a163fb89166d68af61e86d2eda1134454fff38
0qr <69781600+0qr@users.noreply.github.com>
2026-08-04 12:09:24 -0400

    (sorting)

--- models/cb.js
+++ models/cb.js
@@ -242,6 +242,7 @@ let cb0Primary = [
     "monika_reed1",
     "mariameella",
     "dakota_blare",
+    "mia_gothh",
 
     // strictly eye candy, 0-4
     "cindy_luv",
@@ -278,7 +279,6 @@ let cb1Primary = [
     "merryl_clark",
     "staceyblaine",
 
-    "mia_gothh",
     "_june",
     "victoriahouston",
     "unnabella",
